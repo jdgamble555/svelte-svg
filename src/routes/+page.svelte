baf8966d5f6ea1ecfcf4234efcc8592bd3e3979d
Jonathan Gamble <jdgamble555@gmail.com>
2022-12-23 21:07:29 -0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { browser } from '$app/environment';
+	import Test from '../components/test.svelte';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<br />
+{#if browser}
+	<Test />
+{/if}
+<Test />
